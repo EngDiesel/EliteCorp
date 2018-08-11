3adb7e24610bede33a3e1996c9b98355f2e1f1ba
Mostafa Yasin <mostafa.ys2014@gmail.com>
2018-08-11 17:32:58 +0200

    Slider.
    Added the Slider Section and it's pictures.
    retouched some components on the navbar.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/*global $*/
+/*global $, window*/
 $(function () {
     'use strict';
     // start Navbar
@@ -7,7 +7,16 @@ $(function () {
             $(this).addClass('active').siblings().removeClass('active');
         });
     });
-
-
     // End Navbar
+
+    // Start Slider
+    function adjustSliderHieght() {
+        var winH = $(window).height(),
+            upperH = $('.upper-bar').innerHeight(),
+            navH = $('.navbar').innerHeight();
+        $('.slider, .carousel-item').height(winH - (upperH + navH));
+    }
+    adjustSliderHieght();
+
+    // End Slider
 });
