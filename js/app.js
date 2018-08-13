97262aeff361d6e9fdac7a93318db45ead5e02b0
Mostafa Yasin <mostafa.ys2014@gmail.com>
2018-08-13 05:00:51 +0200

    Featured work update.updated the styles of filter buttons,
    updated the styles of filter buttons,
    the shuffle script is now working,
    optimized less Mixins.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 /*global $, window*/
 $(function () {
     'use strict';
-    // start Navbar
-    $('.nav-item').each(function () {
-        $(this).click(function () {
-            $(this).addClass('active').siblings().removeClass('active');
+    // Start Global
+    function activeClassHandeler(className) {
+        $(className).each(function () {
+            $(this).click(function () {
+                $(this).addClass('active').siblings().removeClass('active');
+            });
         });
-    });
+    }
+    // End Global
+
+    // start Navbar
+    activeClassHandeler('.nav-item');
     // End Navbar
 
     // Start Slider
@@ -18,6 +24,23 @@ $(function () {
     }
     adjustSliderHieght();
     // End Slider
+
+    // Start Shuffle
+    activeClassHandeler('.work ul li');
+
+    $('.work ul li').on('click', function () {
+        var dataClass = $(this).data('class');
+
+        if (dataClass === 'all') {
+            //$('.work .shuffle-images .col-md').css('opacity', '1');
+            $('.work .shuffle-images .col-md').fadeTo('slow', 1);
+        } else {
+            $('.work .shuffle-images .col-md').fadeTo('slow', 0.1);
+            $('.work .shuffle-images .col-md ' + dataClass).parent().fadeTo('slow', 1);
+        }
+
+    });
+    // End Shuffle
 
 
 });
